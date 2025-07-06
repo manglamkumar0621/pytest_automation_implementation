325eae9674de43809060d46a53965e5f23374d81
manglamkumar0621 <manglamkumar0621@gmail.com>
2025-07-06 23:31:49 +0530

    Added chrome options to run in headless mode

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -26,6 +26,11 @@ class Driverfactory:
         """Return the chrome options instance."""
         chrome_options = Options()
         chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--window-size=1920,1080")
         prefs = {
             # False disables the Google password manager service.
             "credentials_enable_service": False,
